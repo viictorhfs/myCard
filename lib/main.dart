2f5876ab07aea0d05fb2d143771e5c9b3e0bc8d1
Victor H. Farias <victorsantos.hf@gmail.com>
2026-02-12 15:46:27 -0300

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.deepOrange,
-        body: Container(),
+        body: SafeArea(
+            child: Container(
+              padding: EdgeInsets.all(20.0),
+              margin: EdgeInsets.only(left: 30.0),
+              height: 100.0,
+              width: 100.0,
+              color: Colors.white,
+              child: Text('Olá pessoas!'),
+            )
+        ),
       ),
-    ),
-  );
+    );
+  }
 }

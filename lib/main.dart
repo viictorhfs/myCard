700983f2d8057fade52a40563b51d058bce12ec6
Victor Farias <victorsantos.hf@gmail.com>
2026-02-18 21:34:10 -0300

    card continuos

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,59 @@ class MeuApp extends StatelessWidget {
                   color: Colors.deepOrange.shade100,
                   fontFamily: 'Source Sans 3',
                   letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                    children:
+                    [
+                      Icon(
+                          Icons.phone,
+                          size: 25.0,
+                          color: Colors.deepOrange
+                      ),
+                      SizedBox(
+                        width: 10.0,
+                      ),
+                      Text(
+                          '555 16 991883412',
+                          style: TextStyle(
+                            color: Colors.deepOrange,
+                            fontFamily: 'Source Sans 3',
+                            fontSize: 20.0
+                          ),
+                      ),
+                    ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children:
+                  [
+                    Icon(
+                        Icons.mail,
+                        size: 25.0,
+                        color: Colors.deepOrange
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'email@email.com',
+                      style: TextStyle(
+                          color: Colors.deepOrange,
+                          fontFamily: 'Source Sans 3',
+                          fontSize: 20.0
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
